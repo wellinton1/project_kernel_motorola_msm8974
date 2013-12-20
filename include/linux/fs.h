9523663155551b9672c57c4e59d3de9d71f006b6
Christoph Hellwig <hch@infradead.org>
2013-12-20 05:16:39 -0800

    fs: add a set_acl inode operation
    This will allow moving all the Posix ACL handling into the VFS and clean
    up tons of cruft in the filesystems.
    
    Change-Id: I99d1ac617acb0da73722f0f977357f6a1ed4efab
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1678,6 +1678,7 @@ struct inode_operations {
 	int (*update_time)(struct inode *, struct timespec *, int);
 	struct file *(*open) (struct dentry *, struct file *,
 			      const struct cred *);
+	int (*set_acl)(struct inode *, struct posix_acl *, int);
 } ____cacheline_aligned;
 
 struct seq_file;
